258b0648f55531163a2ad0c268c9ef3eece9d305
tuandat <huynhtuandat05december@gmail.com>
2021-04-28 18:10:34 +0700

    deploy

--- server.js
+++ server.js
@@ -4,23 +4,16 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const userRouter = require('./routers/userRouter')
 const noteRouter = require('./routers/noteRouter')
+const path = require('path')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.use('/users', userRouter);
 app.use('/api/notes', noteRouter);
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
-
-
-app.get('/', (req, res) => {
-    res.json('hello')
-})
 
 
 const URI = process.env.MONGODB_URL;
@@ -31,3 +24,16 @@ mongoose.connect(URI, {
     useFindAndModify: false
 
 }).catch(err => console.log(err.reason));
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
